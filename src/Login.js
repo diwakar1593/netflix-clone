00fe9acb1ea2991d44376110eccb1ae614f6eaf5
diwakar1593 <diwakar200kumar@gmail.com>
2023-04-09 00:27:13 +0530

    commit

--- src/Login.js
+++ src/Login.js
@@ -5,12 +5,12 @@ function Login() {
     <div className="login">
         <div className="login__header">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1200px-Netflix_2015_logo.svg.png" alt='logo' className='login__logo'/>
-
+            
+        </div>
+        <select id="ed547ddee6cc7" className="LanguageSelect" data-uia="language-picker-header"><option selected="" lang="en" label="English" value="en-IN">English</option><option lang="hi" label="हिन्दी" value="hi-IN">हिन्दी</option></select>
             <button className="login__button">
                 Sign In
             </button>
-        </div>
-
         <div className="login__body">
             <h1>Unlimited movies, TV shows and more.</h1>
             <h2>Watch anywhere. Cancel anytime.</h2>
