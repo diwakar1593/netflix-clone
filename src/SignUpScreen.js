fba32c6741f5b086e7b14b45ddb6ea354fb8a934
diwakar1593 <diwakar200kumar@gmail.com>
2023-04-13 01:02:40 +0530

    commit

--- src/SignUpScreen.js
+++ src/SignUpScreen.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import "./css/Login.css"
 function SignUpScreen() {
   return (
-    <div>
-        Hello
+    <div className="signUpScreen">
+        <form>
+          <h1>Sign In</h1>
+          <input type="email" placeholder="Email"/>
+          <input type="password" placeholder="Password"/>
+          <button>Sign In</button>
+
+          <h4>New to Netflix <span>Sign Up Now.</span></h4>
+        </form>
     </div>
   )
 }
